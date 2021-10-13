dd5393007ce823ad38e000e94db52a2f6f4f2e37
Erika <erika@trackchain.mx>
2021-10-12 23:15:46 -0500

    ADD: Sigmoid activation function

---
+++ utils/activation_functions.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Sigmoid():
+    def __call__(self, x):
+        return 1 / (np.exp(-x))
+    
+    def grad(self, x):
+        return self.__call__(x) * (1 - self.__call__(x))
