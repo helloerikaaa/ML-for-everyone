25f0c7631aeb7ca1ca8e62329e80f0e917f2e718
Erika <esanchezf1201@alumno.ipn.mx>
2021-10-13 18:53:41 -0500

    UPDATE: Lint for CNN models

--- convolutional_networks/alexnet.py
+++ convolutional_networks/alexnet.py
@@ -5,18 +5,22 @@ import torch.nn.functional as F
 class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels= 96, kernel_size= 11, stride=4, padding=0 )
+        self.conv1 = nn.Conv2d(
+            in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=0)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5, stride= 1, padding= 2)
-        self.conv3 = nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride= 1, padding= 1)
-        self.conv4 = nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.fc1  = nn.Linear(in_features= 9216, out_features= 4096)
-        self.fc2  = nn.Linear(in_features= 4096, out_features= 4096)
-        self.fc3 = nn.Linear(in_features=4096 , out_features=10)
+        self.conv2 = nn.Conv2d(
+            in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.conv3 = nn.Conv2d(
+            in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(
+            in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(
+            in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(in_features=9216, out_features=4096)
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.fc3 = nn.Linear(in_features=4096, out_features=10)
 
-
-    def forward(self,x):
+    def forward(self, x):
         x = F.relu(self.conv1(x))
         x = self.maxpool(x)
         x = F.relu(self.conv2(x))
