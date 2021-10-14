0607dc64e36356ef5d80656a30070efd33530c5a
Erika <esanchezf1201@alumno.ipn.mx>
2021-10-13 21:26:42 -0500

    REFACTOR: class per each activation function

---
+++ activation_functions/relu.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class ReLU():
+    def __call__(self, x):
+        return np.where(x >= 0, x, 0)
+
+    def gradient(self, x):
+        return np.where(x >= 0, 1, 0)
---
+++ activation_functions/softmax.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Softmax():
+    def __call__(self, x):
+        e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+        return e_x / np.sum(e_x, axis=-1, keepdims=True)
+
+    def gradient(self, x):
+        p = self.__call__(x)
+        return p * (1 - p)
